5891fbc72a2565bd9e3d60b518768c6204f26394
Saurabh Shahane <65170210+SaurabhShahaneG@users.noreply.github.com>
2022-02-16 09:43:37 +0530

    Add files via upload

---
+++ Important_postgres_queries.sql
@@ -0,0 +1,34 @@
+-- get select queries for all tables in database
+select ' select  '''|| tablename  ||''', count(*) from ' || tablename ||' 
+union' from pg_tables where schemaname='public';
+
+-- check postgres version
+select version();
+
+-- check if databasw is in recovery mode or NOT
+select pg_is_in_recovery();
+
+-- to switch wal to the archive
+select pg_switch_wal();
+
+-- see the current lsn of wal
+select pg_current_wal_lsn();
+
+-- see the current wal file into which database is writing
+-- select pg_walfile_name('0/12000148');
+select pg_walfile_name(pg_current_wal_lsn());
+
+-- start the basebackup
+select pg_start_backup('backup');
+
+-- stop the basebackup
+select pg_stop_backup();
+
+-- upgrade postgis extension
+SELECT PostGIS_Extensions_Upgrade();
+
+-- postgis version check
+SELECT postgis_full_version();
+
+-- wal replay paused check. recovery control function
+select pg_is_wal_replay_paused();
